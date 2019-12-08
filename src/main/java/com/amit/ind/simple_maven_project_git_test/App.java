df9a0bcb91c4c14465193acb24215530ec604818
amit <akvhbti606@gmail.com>
2019-12-09 01:46:39 +0530

    checking maven project

---
+++ src/main/java/com/amit/ind/simple_maven_project_git_test/App.java
@@ -0,0 +1,13 @@
+package com.amit.ind.simple_maven_project_git_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
